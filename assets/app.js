c4993eae3483e98badab9424fdc704b37b26fa4b
Kevin Wu <peiyuwu9@gmail.com>
2020-01-17 13:50:36 +0800

    Change font and add copy email function

--- assets/app.js
+++ assets/app.js
@@ -2,26 +2,29 @@ $(document).ready(function () {
     // Initialize Tooltip
     $('[data-toggle="tooltip"]').tooltip();
 
-    // function copyEmail() {
-
-    //     /* Get the text field */
-    //     var copyText = document.getElementById("myInput");
-
-    //     /* Select the text field */
-    //     copyText.select();
-    //     copyText.setSelectionRange(0, 99999); /*For mobile devices*/
-
-    //     /* Copy the text inside the text field */
-    //     document.execCommand("copy");
-
-    //     /* Alert the copied text */
-    //     // alert("Copied the text: " + copyText.value);
-
-    // };
-
     $(".up-arrow").on("click", function () {
         document.body.scrollTop = 0;
         document.documentElement.scrollTop = 0;
+    })
+
+    $(".fa-envelope").on("click", function () {
+        // Create new element
+        const el = document.createElement("input");
+        // Set value (string to be copied)
+        el.setAttribute("value", "peiyuwu9@gmail.com");
+
+        document.body.appendChild(el);
+        // Select text inside element
+        el.select();
+        // Copy text to clipboard
+        document.execCommand('copy');
+        // Remove temporary element
+        document.body.removeChild(el);
+
+        const text = document.getElementById("copy-text");
+        text.innerHTML = "Email Copied";
+
+        setTimeout(function(){ text.innerHTML = "Copy Email"; }, 3000)
     })
 
     AOS.init();
